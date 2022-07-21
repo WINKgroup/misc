ee6fa1c00bd7290dce22e2e9cb4c6578489bc334
Tamer Alkım Tokuç <62282805+althryenix@users.noreply.github.com>
2022-07-21 17:26:09 +0200

    t Published version

---
+++ build/index.d.ts
@@ -0,0 +1,7 @@
+export declare function timeFromNow(ISO: string): string;
+export declare function shrinkUrl(url: string, softMaxLength?: number): string;
+export declare function byteString(fileSize: number): string;
+export declare function isHtml(text: string): boolean;
+export declare function randomId(): string;
+export declare function padZeros(value: number | string, dim: number): string;
+export declare function collapse(text: string, limit?: number): string;
