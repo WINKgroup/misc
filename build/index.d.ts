83620111679e6525281003416ead80b7244f05f5
Domenico Pontari <fairsayan@gmail.com>
2023-01-10 17:34:17 +0100

    no build

--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function timeFromNow(ISO: string): string;
-export declare function shrinkUrl(url: string, softMaxLength?: number): string;
-export declare function byteString(fileSize: number): string;
-export declare function padZeros(value: number | string, dim: number): string;
-export declare function collapse(text: string, limit?: number): string;
-export declare function pickRandom(list: string[]): string | null;
-export declare function camelToTitle(camelCase: string): string;
--- build/node.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function question(question: string): Promise<string>;
-export declare function getKeypress(): Promise<unknown>;
